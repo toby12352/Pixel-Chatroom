f91e67b61ca9fe39fd1c008a264a8bb33e06d7cc
Tun Aung Thaung <alphatrole@gmail.com>
2023-09-27 15:18:05 -0700

    Not correctly working yet, creating a chatroom

---
+++ server.js
@@ -0,0 +1,35 @@
+// Server (Node.js)
+const server = require('http').createServer();
+const cors = require('cors');
+const io = require('socket.io')(server, {
+    cors:{
+        origin:"http://localhost:3000",
+        methods:['GET', 'POST'],
+    }
+});
+
+const connectedUsers = [];
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  const username = `User${Math.floor(Math.random() * 1000)}`;
+
+  io.emit('set-username', username);
+
+  connectedUsers[username] = socket;
+
+  socket.on('client-message', (message) => {
+    // Broadcast the message to all connected clients
+    io.emit('server-message', { username, message });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+    delete connectedUsers[username];
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
     Helmet,
     HelmetProvider
 } from "react-helmet-async";
-// import { Link } from "react-router-dom";
+import "./index.css"
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3001');
 
 export const Home = () => {
+    const [ messages, setMessages ] = useState([]);
+    const [ newMessage, setNewMessage ] = useState('');
+    const [ username, setUsername ] = useState('');
+
+    useEffect(() => {
+        socket.on('set-username', (user) => {
+            setUsername(user);
+        });
+
+        socket.on('message', (message) => {
+            setMessages((prevMessages) => [...prevMessages, message]);
+        });
+
+        return () => {
+            socket.disconnect();
+        }
+    }, []);
+
+    const sendMessage = () => {
+        socket.emit('client-message', newMessage);
+        setNewMessage('');
+    };
+
     return(
         <HelmetProvider>
             <Helmet>
@@ -14,6 +40,29 @@ export const Home = () => {
 
             </Helmet>
             <h1>Home Page!</h1>
+            <div className="chatroom d-inline">
+                <div className="chatbox layout">
+                    {messages.map((msg, index) => (
+                        <div key={index}>
+                            <strong>{msg.username}</strong>: {msg.message}
+                        </div>
+                    ))}
+                </div>
+                <div className="user-input layout">
+                    <input 
+                        className="message-box" 
+                        type="text" 
+                        value={newMessage}
+                        onChange={(e) => setNewMessage(e.target.value)}
+                    />
+                    <button 
+                        className="send-message"
+                        onClick={sendMessage}    
+                    >
+                        Send
+                    </button>
+                </div>
+            </div>
         </HelmetProvider>
     );
 }
